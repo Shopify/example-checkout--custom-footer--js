32dea0402b2e3f2861d28aebcf04788f1ce93156
Greg Craft <greg.craft@shopify.com>
2024-01-26 11:32:53 -0600

    Create custom footer extension for checkout and thank you pages

--- extensions/custom-footer/src/ThankYou.js
+++ extensions/custom-footer/src/ThankYou.js
@@ -3,7 +3,7 @@ import {extension} from '@shopify/ui-extensions/checkout';
 import renderExtension from './Extension.js';
 
 export default extension(
-  'purchase.checkout.footer.render-after',
+  'purchase.thank-you.footer.render-after',
   (root, {shop}) => {
     renderExtension({root, shop});
   }
